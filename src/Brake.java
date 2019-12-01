dc9c4d62ac50145b2262408295fa24a5a5319d36
Denis Spiridonov <denisspir@mail.ru>
2019-12-01 14:09:13 +0300

    +singleton

--- src/Brake.java
+++ src/Brake.java
@@ -1,10 +1,17 @@
 public class Brake {
+    private static Brake instance;
     private int number;
     private String type;
     private String info=("number of brake-"+number+" type brake-"+type);
-    public Brake(int number,String type){
+    private Brake(int number,String type){
         this.number=number;
         this.type=type;
+    }
+    public static Brake getInstance(int number,String type){
+        if (instance == null) {
+            instance = new Brake(number,type);
+        }
+        return instance;
     }
     public String getInfo(){
         return info;
--- src/Director.java
+++ src/Director.java
@@ -2,19 +2,19 @@ public class Director {
     public void constructCrossCountryBike(Builder builder){
         builder.setType(Type.CROSS_COUNTRY);
         builder.setNumberOfGears(21);
-        builder.setBrake(new Brake(2,"mechanical"));
+        builder.setBrake(Brake.getInstance(2,"mechanical"));
         builder.detSuspension(new Suspension("soft","hard"));
     }
     public void constructRacingBike(Builder builder){
         builder.setType(Type.RACING);
         builder.setNumberOfGears(16);
-        builder.setBrake(new Brake(2,"hydraulic"));
+        builder.setBrake(Brake.getInstance(2,"hydraulic"));
         builder.detSuspension(new Suspension("hard","hard"));
     }
     public void constructBmxBike(Builder builder){
         builder.setType(Type.BMX);
         builder.setNumberOfGears(1);
-        builder.setBrake(new Brake(0,"foot"));
+        builder.setBrake(Brake.getInstance(0,"foot"));
         builder.detSuspension(new Suspension("hard","hard"));
     }
 }
